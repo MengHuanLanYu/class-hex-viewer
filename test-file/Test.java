dec7c5d01ec163341d214beaefc8daf1c94478f9
MengHuanLanYu <15076829795@163.com>
2021-06-22 15:15:33 +0800

    提交测试文件

---
+++ test-file/Test.java
@@ -0,0 +1,21 @@
+import java.io.Serializable;
+
+public class Test implements Serializable{
+	public static void main(String[] args){
+		String abc = "Hello World!";
+		System.out.println(abc);
+		//long ta = 55l;
+		//System.out.println(ta);
+		//int c = 9953;
+		//System.out.println(c);
+		//double w = 45.2;
+		//System.out.println(w);
+		//float aq = 23.0f;
+		//System.out.println(aq);
+		//System.out.println(addMethod(5,9));
+	}
+	
+//	private static int addMethod(int a , int b){
+	//	return a + b;
+	//}
+}
