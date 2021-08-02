45e4c9ae0bb9c9771d045395918abe2f980e374f
MengHuanLanYu <15076829795@163.com>
2021-08-02 15:01:00 +0800

    modify FloatInfo.java

--- hex-parsing/src/main/java/top/liu15/constant/FloatInfo.java
+++ hex-parsing/src/main/java/top/liu15/constant/FloatInfo.java
@@ -1,13 +1,16 @@
 package top.liu15.constant;
 
+import lombok.Getter;
 import top.liu15.datatype.ByteReader;
 import top.liu15.datatype.U4;
+import top.liu15.datatype.UnsignedNumber;
 
 /**
  * @author lhy
  * @version 1.0
  * @date 2021/6/3 23:06
  */
+@Getter
 public final class FloatInfo extends ConstantInfo {
 
     private U4 bytes;
@@ -16,8 +19,9 @@ public final class FloatInfo extends ConstantInfo {
     @Override
     public void readDescription(ByteReader reader) {
         // 读取高位在前的float值
-        this.bytes = new U4(reader);
+        this.bytes = new U4(reader, UnsignedNumber.LONG_TO_HEX);
+        // https://docs.oracle.com/javase/specs/jvms/se8/html/jvms-4.html#jvms-4.4.4
         // 填充自己
-        super.setDescription(this.bytes.getDescription());
+        super.setDescription(String.valueOf(Float.intBitsToFloat(this.bytes.getValue().intValue())));
     }
 }
--- hex-parsing/src/main/java/top/liu15/constant/IntegerInfo.java
+++ hex-parsing/src/main/java/top/liu15/constant/IntegerInfo.java
@@ -1,13 +1,16 @@
 package top.liu15.constant;
 
+import lombok.Getter;
 import top.liu15.datatype.ByteReader;
 import top.liu15.datatype.U4;
+import top.liu15.datatype.UnsignedNumber;
 
 /**
  * @author lhy
  * @version 1.0
  * @date 2021/6/3 23:03
  */
+@Getter
 public final class IntegerInfo extends ConstantInfo {
 
     private U4 bytes;
@@ -15,8 +18,8 @@ public final class IntegerInfo extends ConstantInfo {
     @Override
     public void readDescription(ByteReader reader) {
         // 读取高位在前的int值
-        this.bytes = new U4(reader);
+        this.bytes = new U4(reader, UnsignedNumber.LONG_TO_HEX);
         // 填充自己
-        super.setDescription(this.bytes.getDescription());
+        super.setDescription(String.valueOf(this.bytes.getValue()));
     }
 }
